eaa1fbe9e3f6786955d5b515539ebe3d8ce8909c
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-05-10 16:36:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,19 +9,24 @@ const fetchPokemon = (pokemon) => {
 
     Promise.all(pokemonPromises)
         .then(pokemons => {
-            console.log(pokemons)
+            // console.log(pokemons)
 
             const lisPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
                 accumulator += `
                     <li class="card">
+                    <img class="card-image ${types[0]} alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
                         <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
-                        <p class=""></p> 
+                        <p class="card-subtitle">${types.join(' | ')}</p> 
                     </li>
                 `
                 return accumulator
             }, '')
 
-            console.log(lisPokemons)
+            const ul = document.querySelector("[data-js='pokedex']")
+
+            console.log(ul)
         })
     
 }
